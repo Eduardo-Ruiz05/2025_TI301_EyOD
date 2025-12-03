ff4c44ce7942d869355b47285b20862125547c3e
Eduardo-Ruiz05 <eduardo.ruizaguilar.50@gmail.com>
2025-12-03 11:15:23 -0600

    Algoritmos de estructuras NoLineales: Recursividad

--- src/tema3/estructurasNoLineales/recursividad/RecursionLineal.java
+++ src/tema3/estructurasNoLineales/recursividad/RecursionLineal.java
@@ -20,4 +20,71 @@ public class RecursionLineal {
             // hacia atrás, además de sumar el próximo return con el actual 
             // (el valor en la posición n-1)
     }
+    
+    /*
+    * Descripción: Calcula el valor de x a la n-écima potencia 
+    * (para enteros no negativos).
+    * retorna: double r.
+    */
+    public static double powerBad(double x, int n) {
+        if(n == 0)
+            return 1;   //todo número positivo elevado a la 0 es igual a 1
+        else
+            return x * powerBad(x, n-1); //recur n veces (multiplica x n veces)
+    }
+    
+    /*
+    * Descripción: Calcula el valor de x a la n-écima potencia (n >= 0)
+    * en lugar de hacer recur n veces, la cantidad de veces que
+    * se hará recur nuevamente se reducirá a la mitad por cada recur.
+    * retorna: double result.
+    */
+    public static double powerGood(double x, int n) {
+        if(n == 0)
+            return 1;   //todo número positivo elevado a la 0 es igual a 1
+        
+        else{
+            double partial = powerGood(x, n/2); //recur con n a la mitad,
+
+            double result = partial * partial; //se eleva n/2 ó k
+            
+            if(n % 2 == 1)
+                result *= x;   //si n es impar entonces se suma 1 al exponente
+            
+            return result;
+        }
+    }
+    
+    /*
+    * Descripción: Genera la serie fibonacci en la posición n (forma 
+    * ineficiente)(index desde 0).
+    *
+    * retorna: long F(n)
+    */
+    public static long fibonacciBad(int n) {
+        if(n <= 1)
+            return n;   //la posición 0 y 1 de la serie es 1
+        else
+            return fibonacciBad(n-2) + fibonacciBad(n-1);
+                    //suma el valor anterior a n con el anterior a ese mismo
+    }
+    
+    /*
+    * Descripción: Genera un par de valores de la serie fibonacci en la posición
+    * n y n-1. Si n > 1: se hace recur disminuyendo n en 1 y guardando ese valor
+    * en el long temporal, se usará posteriormente para encontrar
+    * el valor F(n) sumando ambas posiciones de temp
+    * retorna: long[] {F(n), F(n-1)}
+    */
+    public static long[] fibonacciGood(int n) {
+        if(n <= 1){
+            long[] answer = {n,0};  
+            return answer;
+        } else{
+            long[] temp = fibonacciGood(n-1);
+            long[] answer = {temp[0] + temp[1], temp[0]};
+            return answer;
+        }
+    }
 }
+
--- src/tema3/estructurasNoLineales/recursividad/RecursionTest.java
+++ src/tema3/estructurasNoLineales/recursividad/RecursionTest.java
@@ -1,13 +1,41 @@
 
 package tema3.estructurasNoLineales.recursividad;
 
-import static tema3.estructurasNoLineales.recursividad.RecursionLineal.linearSum;
+import static tema3.estructurasNoLineales.recursividad.RecursionLineal.*;
 
 
 public class RecursionTest {
 
     
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        System.out.println("Caso fibonacciBad: " + fibonacciBad(45));
+        
+        long endTime = System.nanoTime() - startTime;
+
+        System.out.println("Tiempo de ejecución: " + (endTime / 1_000_000.0) + " milisegundos");
+        
+        System.out.print("Caso fibonacciGood: ");
+        
+        long[] array;
+        
+        startTime = System.nanoTime();
+
+        array = fibonacciGood(45);
+        
+        endTime = System.nanoTime() - startTime;
+        
+        System.out.println("{" + array[0] + ", " + array[1] + "}");
+        
+        System.out.println("Tiempo de ejecución: " + (endTime / 1_000_000.0) + " milisegundos");
+        
+        
+        /*
+        System.out.println("Caso: bad algoritm\n 10^10000 = " + powerBad(10,10000));
+        
+        System.out.println("Caso: good algoritm\n 10^10000 = " + powerGood(10,10000));
+        
         int[] array1 = {2, 1, 0, 6, 8, 10};
         System.out.println("caso 1: (2+1+0+6) = " + linearSum(array1, 4));
         
@@ -18,6 +46,7 @@ public class RecursionTest {
         System.out.println("\ncaso 3: promedio de las primeras 4 calificaciones \n" 
                 + linearSum(array3, 4)/4);
         //Resultado esperado: 84 (sin mostrar decimal)
+        */
     }
     
 }
